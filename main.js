120dd9e5651b20e45f388c59b9ee269bca017c34
huyhaf <huyhn303@gmail.com>
2023-07-28 18:34:26 +0700

    update noi dung

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-var input=document.querySelector('#input')
-console.log(input)
+const inputElement = document.querySelector('#InputField');
+const typeElement= document.querySelector('#Temptype');
+const resultvalue=document.querySelector('#Result');
+const convert=document.querySelector('#convert')
+
+var inputvalue,typevalue;
+
+inputElement.oninput= function(e){
+    inputvalue=e.target.value;
+}
+
+typeElement.onchange = function(e){
+    typevalue=e.target.value;
+}
+
+convert.addEventListener('click',(e) => {
+    e.preventDefault();
+    converttoC();
+})
+
+function converttoC(){
+    if(typevalue==='F degree'){
+        resultvalue.innerHTML = `${((Number.parseInt(inputvalue)-32)*(5/9)).toFixed(2)}`;
+    }
+    else if(typevalue==='K degree'){
+        resultvalue.innerHTML = `${(Number.parseInt(inputvalue)-273.15).toFixed(2)}`;
+    }
+}
+
+
